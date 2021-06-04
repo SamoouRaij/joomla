63bbc4cb14a58837f25c2efe1ec9336d078daae0
Artem <84867564+SamoouRaij@users.noreply.github.com>
2021-06-04 12:04:28 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,37 +1,34 @@
-#!/bin/bash
-
-source "$PWD"/config.shlib;
 #!bin/bash
-
-## Configuration
+#Firs Conf
+#rm -rf tmp/*
 user=admin
 domain="$(cat /etc/hostname)"
+#Load library func
+source /home/$user/web/$domain/joomla/config.shlib;
+
+#Site directory configuration
+SITEDIR="$(config_get $SITEDIR)"
+
+echo "########################################################"
+echo "########################################################"
 
 export VESTA=/usr/local/vesta/
-
-############
-
-## Script
-
-# FOLDER_NAME="USER INPUT"
 SITEDIR="/home/$user/web/$domain/public_html"
 
 rm -rf $SITEDIR/*
 
 JOOMLA_PACKAGE=Joomla_3-9-27-Stable-Full_Package.zip
 
-# Change to webdir
+# Change to dir
 cd $SITEDIR/
 
-# Get Joomla Version
+# Get & unzip Joomla_latest
 wget https://downloads.joomla.org/us/cms/joomla3/3-9-27/$JOOMLA_PACKAGE
-
-# Unzip Jooomla Version
 unzip $JOOMLA_PACKAGE
 
 printf "\mkdir Joomla successfull\n"
 
-DBUSERSUFB="joomla"
+DBUSERSUFB="joo";
 i=0;
 while [ $i -lt 99 ]
 do
@@ -44,70 +41,46 @@ fi
 done
 PASSWDDB=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 
-
 /usr/local/vesta/bin/v-add-database $user $DBUSERSUF $DBUSERSUF $PASSWDDB mysql
 
-# Function jinstall
-jinstall () {
+# Function install
+        # Place install.sql to sql mysql installation folder
+        cp /home/$user/web/$domain/joomla/jinstaller.sql $SITEDIR/installation/sql/mysql/
+#
+        DBPREFIX="joo"
+#    #
+#        # Go to installation mysql folder
+        cd installation/sql/mysql/
+#        # Add install.sql to joomla.sql
+        cat jinstaller.sql >> joomla.sql
+#
+#        # Set Prefix
+sed -i -e "s/#__/$DBPREFIX/g" joomla.sql
+        # MySQL Import
+        mysql -uroot $DBUSER < joomla.sql
 
-	# Place jinstaller.sql to sql mysql installation folder
-	cp "$PWD"/jinstaller.sql $SITEDIR/installation/sql/mysql/
-	
-	DBHOST=localhost
-	DBPREFIX="joomla"
-    #
-	# Go to installation mysql folder
-	cd installation/sql/mysql/
+        # Go to installation folder
+        cd $SITEDIR/installation/
 
-	# Add jinstaller.sql to joomla.sql
-	cat jinstaller.sql >> joomla.sql
+        # Set Configuration.php
 
-	# Set Prefix
-	sed -i -e "s/#__/$DB_PREFIX/g" joomla.sql
-
-	# MySQL Import
-	mysql -uroot $DBUSERSUF < joomla.sql
-
-	# Go to installation folder
-	cd $SITEDIR/installation/
-
-	# Set Configuration.php
-	sed -i -e "s/host = \x27localhost\x27/host = \x27$DBHOST\x27/g" configuration.php-dist
-	sed -i -e "s/user = \x27\x27/user = \x27$DBUSERSUF\x27/g" configuration.php-dist
+#       sed -i -e "s/host = \x27localhost\x27/host = \x27$DB_HOST\x27/g" configuration.php-dist
+	sed -i -e "s/user = \x27\x27/user = \x27$DBUSER\x27/g" configuration.php-dist
 	sed -i -e "s/password = \x27\x27/password = \x27$PASSWDDB\x27/g" configuration.php-dist
-	sed -i -e "s/db = \x27\x27/db = \x27$DBUSERSUF\x27/g" configuration.php-dist
+	sed -i -e "s/db = \x27\x27/db = \x27$DBUSER\x27/g" configuration.php-dist
 	sed -i -e "s/dbprefix = \x27jos_\x27/dbprefix = \x27$DBPREFIX\x27/g" configuration.php-dist
-
-	sed -i -e "s/tmp_path = \x27\/tmp\x27/tmp_path = \x27html\/$SITEDIR\/tmp\x27/g" configuration.php-dist
-	sed -i -e "s/log_path = \x27\/var\/logs\x27/log_path = \x27html\/$SITEDIR\/administrator\/logs\x27/g" configuration.php-dist
-
-	# Place configuration.php
-	cp configuration.php-dist $SITEDIR/configuration.php-dist
-	cd $SITEDIR
+	sed -i -e "s/tmp_path = \x27\/tmp\x27/tmp_path = \x27$SITEDIR\/tmp\x27/g" configuration.php-dist
+	sed -i -e "s/log_path = \x27\/var\/logs\x27/log_path = \x27$SITEDIR\/administrator\/logs\x27/g" configuration.php-dist
+#html - in sed
+# Place configuration.php
+        cp configuration.php-dist $SITEDIR/configuration.php-dist
+        cd $SITEDIR
     mv configuration.php-dist configuration.php
-	# Function uninstallInstallltaion
+        # Function uninstallInstallltaion
+    
+		rm -rf $SITEDIR/installation
 
-	rm -rf $SITEDIR/installation
-	exit 0
-#
-#
-##hile true; do
-	#rintf "\nDo you want to uninstall installation folder:\n"
-	#ead -p "Answer: ([Y] Yes [N] No)." yn
-	#case $yn in
-#		[Yy]* ) uninstallInstallltaion; break;;#
-	#	[Nn]* ) exit;
-#		* ) echo "Please answer yes or no.";;
-#	esac
-#done
-#
+chown -R $user. $SITEDIR
+chmod -R 755 $SITEDIR
 
-#hile true; do
-#rintf "\nDo you want to install $J_VERSION Database and create configuration.php:\n"
-#ead -p "Answer: ([Y] Yes [N] No)." yn
-#case $yn in
-#	[Yy]* ) jinstall; break;;
-#	[Nn]* ) exit;;
-#	* ) echo "Please answer yes or no.";;
-#esac
-#done
+exit 0 
